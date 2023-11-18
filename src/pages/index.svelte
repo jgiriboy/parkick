522ebf7a7e5d7ff607daf98d89281c0334fb4f0f
Gieun Jeong <gieun.jeong@kaist.ac.kr>
2023-11-18 20:54:23 +0900

    editing mistake

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -10,83 +10,70 @@
     });
 </script>
 
-<div class="e15_8">
-    <div class="e39_370">
-        <span class="e21_193 font-medium">3:53</span>
-
-        <div class="wifi-icon">
-            <img src="images/wifi.svg" alt="" />
+<main class="index-main">
+    <nav class="index-nav">
+        <span class="nav-logo font-semibold">ParKick</span>
+        <div class="nav-menu">
+            <img src="images/coupon.svg" class="nav-menu-item" alt="" />
+            <img src="images/bell.svg" class="nav-menu-item" alt="" />
+            <img src="images/menu.svg" class="nav-menu-item" alt="" />
         </div>
-
-        <div class="signal-icon">
-            <img src="images/signal.svg" alt="" />
-        </div>
-
-        <div class="battery-icon">
-            <img src="images/battery.svg" alt="" />
-        </div>
-    </div>
-
-    <div class="bell-img">
-        <img src="images/bell.svg" alt="" />
-    </div>
-
-    <div class="menu-img">
-        <img src="images/menu.svg" alt="" />
-    </div>
-
-    <div class="coupon-img">
-        <img src="images/coupon.svg" alt="" />
-    </div>
-
-    <div class="ad-img">
-        <img src="images/mainban.png" alt="" />
-    </div>
-
-    <div class="e21_241" />
-    <span class="e21_244 font-bold"
-        >바르게 주차만 해도<br />PK 코인이<br />쌓인다!</span
-    >
-    <span class="e21_246 font-medium">ParKick</span>
-
-    <div class="e21_252">
-        <img src="images/megaphone.svg" alt="" />
-    </div>
-
-    <span class="e21_253 font-regular"
-        >ParKick 출시 기념 PK코인 적립 이벤트 !</span
-    >
-    <a href="/findVehicle-main" class="e21_268">
-        <span class="e21_270 font-semibold">파킹 스테이션 </span>
-        <span class="e39_293">가까운<br />파킹 스테이션을<br />찾아요</span>
-        <div>
-            <div class="e21_277">
-                <img src="images/bicycle.svg" alt="" />
+    </nav>
+    <div class="index-ad">
+        <div class="ad-text">
+            <span class="font-regular">ParKick</span>
+            <span class="font-bold"
+                >바르게 주차만 해도<br />PK 코인이<br />쌓인다!</span
+            >
+            <div class="ad-text-box font-bold">
+                <span>Ride To Earn</span>
             </div>
-        </div></a
-    >
-    <!-- TODO -->
-    <!-- TODO -->
-    <!-- TODO: 294 클릭 시 rent 로 가는 링크를 생성해야 함 -->
-    <a href="/QRscanRent" class="e39_294">
-        <span class="e39_295 font-semibold">대여하기</span>
-        <span class="e39_303">QR 찍고<br />바로 출발해요</span>
-        <div class="e39_317">
-            <img src="images/qrcode.svg" alt="" />
-        </div></a
-    >
-    <span class="e21_269 font-semibold">ParKick</span>
-
-    <div class="e39_321" />
-    <div class="e39_329" />
-    <span class="e39_322 font-semibold">주차하고 리워드 받기 !</span><span
-        class="e39_330 font-semibold">모은 PK 코인</span
-    >
-    <div class="e39_323" />
-    <span class="e39_324">리워드 받기</span>
-    <div class="e39_335" />
-    <span class="e39_336">코인 사용하러 가기</span>
-    <span class="e39_325">흩어져 있는 킥보드를 다시 주차해요</span>
-    <span class="e39_331">OOO 라이더 님</span>
-    <span class="e39_332">{$rewards.currentPoint} 원</span>
-</div>
+        </div>
+        <img src="images/kickboard-woman.png" class="ad-image" alt="" />
+    </div>
+    <div class="index-note">
+        <img src="images/megaphone.svg" class="index-note-icon" alt="" />
+        <div class="index-note-text-border">
+            <span class="index-note-text"
+                >ParKick 출시 기념 PK 코인 적립 이벤트 !</span
+            >
+        </div>
+    </div>
+    <div class="index-boxes">
+        <div class="index-service">
+            <div class="index-parkingStation index-service-box">
+                <span class="font-semibold">파킹 스테이션</span>
+                <span>가까운<br />파킹 스테이션을<br />찾아요</span>
+                <img src="images/bicycle.svg" class="service-img-PS" alt="" />
+            </div>
+            <div class="index-QRcode index-service-box">
+                <span class="font-semibold">대여하기</span>
+                <span>QR 찍고<br />바로 출발해요</span>
+                <img src="images/qrcode.svg" class="service-img-QR" alt="" />
+            </div>
+        </div>
+        <div class="index-get-reward">
+            <div class="get-reward-text">
+                <span class="font-semibold">주차하고 리워드 받기 !</span>
+                <span>흩어져 있는 킥보드를 다시 주차해요</span>
+            </div>
+            <a href="/">
+                <span>리워드 받기</span>
+            </a>
+        </div>
+        <div class="index-user-reward">
+            <div class="user-reward-text">
+                <div class="user-reward-left">
+                    <span class="font-semibold">모은 PK 코인</span>
+                    <span class="font-regular">OOO 라이더 님</span>
+                </div>
+                <div class="user-reward-right">
+                    <span class="font-regular">{$rewards.currentPoint} 원</span>
+                </div>
+            </div>
+            <div class="user-reward-button">
+                <span>코인 사용하러 가기</span>
+            </div>
+        </div>
+    </div>
+</main>
